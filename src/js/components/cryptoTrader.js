8bd5b122c9c1fe9890ddb899e266f6fc0dba49ce
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-09-12 21:38:51 +0330

    {Feat} : ( Add Crypto Traders Section )

---
+++ src/js/components/cryptoTrader.js
@@ -0,0 +1,46 @@
+export function getCryptoTrader() {
+    return `
+ <section id="crypto-section" class=" text-center">
+    <div class="container py-5">
+      <h2 class="section-title ">Become a crypto trader in <br/> seconds</h2>
+      <p class="section-subtitle mt-4  mx-auto">We have everything you need to start trading. We possess a comprehensive database of tools, resources, and information related to trading.</p>
+
+      <div class="row g-4 mt-5 ">
+        <div class="col-lg-4 col-md-6 z-3 ">
+          <div class="crypto-card p-4 d-flex justify-content-center align-items-center flex-column">
+          <img src="./src/assets/Home_assets/append_to.png" alt="learn crypto" class="mt-4  mb-5">
+          <h3 class="card-title">Append to your wallet</h3>
+          <p class="card-text mt-2">When you "append to your wallet," you are extending the functionality or contents of your wallet.</p>
+          <a href="#" class="btn mb-4 ">Add Wallet</a>
+          </div>
+          </div>
+          <div class="col-lg-4 col-md-6 z-3 ">
+          <div class="crypto-card p-4 d-flex justify-content-center align-items-center flex-column">
+          <img src="./src/assets/Home_assets/exchange_assets.png" alt="wallet" class="mt-4  mb-5">
+          <h3 class="card-title">Exchange Assets</h3>
+          <p class="card-text mt-2">The exchange of assets can take place through blockchain-based platforms or relevant exchanges.</p>
+          <a href="#" class="btn mb-4 Exchange-btn">Exchange</a>
+          </div>
+          </div>
+          <div class="col-lg-4 col-md-6 z-3  mx-md-auto">
+          <div class="crypto-card p-4 d-flex justify-content-center align-items-center flex-column">
+          <img src="./src/assets/Home_assets/learn_crypto.png" alt="exchange" class="mt-4  mb-5">
+            <h3 class="card-title">Learn crypto</h3>
+            <p class="card-text mt-2">Crypto emphasizes strong security and privacy measures. Cryptocurrencies use cryptographic technology.</p>
+            <a href="#" class="btn mb-4 ">Learn now</a>
+          </div>
+        </div>
+      </div>
+      <div class="contact-us-btn-wrapper">
+        <a href="#" class="contact-us-btn mt-2 ">Contact us →</a>
+      </div>
+     
+    </div>
+     <div class="myback">
+           <div class="inside-myback">
+      </div>
+      </div>
+  </section>
+       `
+        ;
+}
--- src/js/rezaMain.js
+++ src/js/rezaMain.js
@@ -1,10 +1,8 @@
 // main.js
-import { getHeader } from './components/header.js';
 import { getFooter } from './components/footer.js';
-import { getMainContent } from './components/main-content.js';
+import { getCryptoTrader } from './components/cryptoTrader.js';
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.body.insertAdjacentHTML('afterbegin', getHeader());
-    document.body.insertAdjacentHTML('beforeend', getMainContent());
+    document.body.insertAdjacentHTML('beforeend', getCryptoTrader());
     document.body.insertAdjacentHTML('beforeend', getFooter());
 });
