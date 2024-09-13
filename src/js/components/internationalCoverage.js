5383a254b1f16e43dc24335367bb6cccd9a50dde
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-09-13 16:41:34 +0330

    {Feat} : (Add International Coveregr Section )

---
+++ src/js/components/internationalCoverage.js
@@ -0,0 +1,43 @@
+export function getInternationalCoverege() {
+    return `
+        <section id="InternationalCoverege" class="position-relative container-full">
+        <div class="container ">
+            <div class="row">
+                <div class="col-12 International-titles-container d-flex justify-content-center align-items-center flex-column">
+                    <h2 class="International-tittle text-center">
+                        Unparalleled international coverage 
+                    </h2>
+                        <p class="International-sub-tittle mt-2  mx-auto">
+                         One of Alchemy Pay's notable strengths lies in its emphasis on providing access to emerging markets. By focusing on these markets, Alchemy Pay aims to enable individuals and businesses in these regions to participate in the growing digital economy. 
+                         </p>
+                </div>
+            </div>
+            <!-- end of tittles -->
+
+            <!-- first row -->
+             <div class="row col-12 gap-2 gap-lg-4  mt-5 d-flex justify-content-center mx-auto">
+                <div class="item col-2">
+                    <img src="./src/assets/Home_assets/dana.svg" alt="dana-icon">
+                </div>
+                <div class="item col-2">
+                    <img src="./src/assets/Home_assets/spe.svg" alt="spe-icon">
+                </div>
+                <div class="item col-2">
+                    <img src="./src/assets/Home_assets/pix.svg" alt="pix-icon">
+                </div>
+                <div class="item col-2 mt-4 mt-sm-0">
+                    <img src="./src/assets/Home_assets/applepay.svg" alt="applepay-icon">
+                </div>
+                <div class="item col-2 mt-4 mt-sm-0">
+                    <img src="./src/assets/Home_assets/bca.svg" alt="bca-icon">
+                </div>
+                <div class="item col-2 mt-4 mt-sm-0">
+                    <img src="./src/assets/Home_assets/bdo.svg" alt="bdo-icon">
+                </div>
+            </div>
+            <!-- end first row -->
+        </div>
+    </section>
+           `
+        ;
+}
--- src/js/rezaMain.js
+++ src/js/rezaMain.js
@@ -1,9 +1,11 @@
 // main.js
 import { getFooter } from './components/footer.js';
 import { getCryptoTrader } from './components/cryptoTrader.js';
-import { getSupporting} from './components/supporting.js';
+import { getSupporting } from './components/supporting.js';
+import { getInternationalCoverege } from './components/internationalCoverage.js';
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    document.body.insertAdjacentHTML('beforeend', getInternationalCoverege());
     document.body.insertAdjacentHTML('beforeend', getSupporting());
     document.body.insertAdjacentHTML('beforeend', getCryptoTrader());
     document.body.insertAdjacentHTML('beforeend', getFooter());
