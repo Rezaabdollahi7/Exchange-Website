b625d4abb23b3270c34fe2b5e3f2c1f52be25a92
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-09-13 15:25:45 +0330

    {Feat} : ( Add Supporting section on home page )

---
+++ src/js/components/supporting.js
@@ -0,0 +1,101 @@
+export function getSupporting() {
+    return `
+           <section id="support" class="position-relative container-full" >
+        <div class="container ">
+        <div class="row">
+        <div class="col-12 support-titles-container d-flex justify-content-center align-items-center flex-column">
+        <h2 class="support-tittle text-center">
+        <p>
+        Supporting the forefront of
+        </p>
+       
+        <p class="web3"> Web3 projects </p>
+        
+        </h2>
+        </div>
+        
+        </div>
+        <!-- end of tittles -->
+        
+        <!-- first row -->
+        <div class="row col-12 gap-4 mx-auto d-flex justify-content-center mt-3">
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/coinbase.svg" alt="">
+        </div>
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/dodo.svg" alt="">
+        </div>
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/okx.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex ">
+        <img src="./src/assets/Home_assets/bitrue.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex ">
+        <img src="./src/assets/Home_assets/polygon.svg" alt="">
+        </div>
+        </div>
+        <!-- end first row -->
+        
+        <!-- second row -->
+        <div class="row col-12 gap-2 gap-lg-4  mt-5 d-flex justify-content-center mx-auto">
+        <div class="item col-2">
+        <img src="./src/assets/Home_assets/quickswap.svg" alt="">
+        </div>
+        <div class="item col-2">
+        <img src="./src/assets/Home_assets/kraken.svg" alt="">
+        </div>
+        <div class="item col-2">
+        <img src="./src/assets/Home_assets/cointr.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex">
+        <img src="./src/assets/Home_assets/lbank.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex">
+        <img src="./src/assets/Home_assets/uniswap.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex">
+        <img src="./src/assets/Home_assets/cryptocom.svg" alt="">
+        </div>
+        </div>
+        <!-- end of second row -->
+        
+        <!-- third row -->
+        <div class="row col-12 gap-4 mx-auto d-flex justify-content-center mt-5">
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/befi_wallet.svg" alt="">
+        </div>
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/saasgo.svg" alt="">
+        </div>
+        <div class="item col-3  col-lg-2">
+        <img src="./src/assets/Home_assets/coinw.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex ">
+        <img src="./src/assets/Home_assets/sui.svg" alt="">
+        </div>
+        <div class="item col-2 d-none d-md-flex ">
+        <img src="./src/assets/Home_assets/tptoken.svg" alt="">
+        </div>
+        </div>
+        <!-- end of third row -->
+        
+        <div class="row mt-5">
+        <div class="btn-container mb-4 d-flex justify-content-center align-items-center gap-4">
+        <a class="first" href="#">See all partners →</a>
+        <a class="second" href="#">Talk to us</a>
+        </div>
+        </div>
+        </div>
+        
+        <!-- left & right shadow -->
+        <div class="shadow-img-wrapper position-absolute w-100 d-flex justify-content-between align-items-center">
+        <img class="left"  src="./src/assets/Home_assets/Ellipse-left.png" alt="">
+        <img class="right" src="./src/assets/Home_assets/Ellipse-right.png" alt="">
+            
+        </div>
+        </section>
+        `
+        ;
+    }
+    
--- src/js/rezaMain.js
+++ src/js/rezaMain.js
@@ -1,8 +1,10 @@
 // main.js
 import { getFooter } from './components/footer.js';
 import { getCryptoTrader } from './components/cryptoTrader.js';
+import { getSupporting} from './components/supporting.js';
 
 document.addEventListener('DOMContentLoaded', function() {
+    document.body.insertAdjacentHTML('beforeend', getSupporting());
     document.body.insertAdjacentHTML('beforeend', getCryptoTrader());
     document.body.insertAdjacentHTML('beforeend', getFooter());
 });
