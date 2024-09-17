22c9db5ef5ca673ae75e4ea75cc776cd43e85bf9
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-09-17 23:27:46 +0330

    {Feat} : ( Add Tranding section of exchange page )

---
+++ Exchange/src/js/components/trending.js
@@ -0,0 +1,82 @@
+const trending = `
+    <section id="trending" class=" mt-4">
+        <div class="container-fluid">
+            <h3 class="section-tittle">Trending</h3>
+        </div>
+        <div class="cards-container row mt-3 gap-4 d-flex justify-content-center align-items-center ">
+            <!-- item 1 -->
+            <div class="col-12 col-md-6  col-lg-3 mt-3 card-item d-flex flex-column">
+                <div class="coin-section-container d-flex justify-content-between ">
+                    <div class="coin-details d-flex justify-content-center gap-3 ">
+                        <img class="coin-icon" src="./assets/icons/bitcoin.svg" alt="">
+                        <div class="name d-flex flex-column justify-content-start ">
+                            <p class="coin-complate-name">Bitcoin</p>
+                            <span class="coin-name">BTC</span>
+                        </div>
+                    </div>
+                    <div class="coin-precent">
+                        <span class="green-percent">19%</span>
+                    </div>
+                </div>
+                <div class="coin-chart-container mt-3 ">
+                    <img class="mx-auto" src="./assets/icons/greenchart.svg" alt="">
+                </div>
+            </div>
+            <!-- item 2 -->
+            <div class="col-12 col-md-6  col-lg-3 mt-3 card-item d-flex flex-column">
+                <div class="coin-section-container d-flex justify-content-between ">
+                    <div class="coin-details d-flex justify-content-center gap-3 ">
+                        <img class="coin-icon" src="./assets/icons/theter.svg" alt="">
+                        <div class="name d-flex flex-column justify-content-start ">
+                            <p class="coin-complate-name">Theter</p>
+                            <span class="coin-name">USDT</span>
+                        </div>
+                    </div>
+                    <div class="coin-precent">
+                        <span class="green-percent">15%</span>
+                    </div>
+                </div>
+                <div class="coin-chart-container mt-3 ">
+                    <img class="mx-auto" src="./assets/icons/greenchart.svg" alt="">
+                </div>
+            </div>
+            <!-- item 3 -->
+            <div class="col-12 col-md-6  col-lg-3 mt-3 card-item d-flex flex-column">
+                <div class="coin-section-container d-flex justify-content-between ">
+                    <div class="coin-details d-flex justify-content-center gap-3 ">
+                        <img class="coin-icon" src="./assets/icons/tron.svg" alt="">
+                        <div class="name d-flex flex-column justify-content-start ">
+                            <p class="coin-complate-name">Tron</p>
+                            <span class="coin-name">TRX</span>
+                        </div>
+                    </div>
+                    <div class="coin-precent">
+                        <span class="red-percent">-15%</span>
+                    </div>
+                </div>
+                <div class="coin-chart-container mt-3 ">
+                    <img class="mx-auto" src="./assets/icons/redchart.svg" alt="">
+                </div>
+            </div>
+            <!-- item 4 -->
+            <div class="col-12 col-md-6  col-lg-3 mt-3 card-item d-flex flex-column">
+                <div class="coin-section-container d-flex justify-content-between ">
+                    <div class="coin-details d-flex justify-content-center gap-3 ">
+                        <img class="coin-icon" src="./assets/icons/bluebitcoin.svg" alt="">
+                        <div class="name d-flex flex-column justify-content-start ">
+                            <p class="coin-complate-name">Bitcoin</p>
+                            <span class="coin-name">BTC</span>
+                        </div>
+                    </div>
+                    <div class="coin-precent">
+                        <span class="green-percent">25%</span>
+                    </div>
+                </div>
+                <div class="coin-chart-container mt-3 ">
+                    <img class="mx-auto" src="./assets/icons/greenchart.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+`
+export { trending }
--- Exchange/src/main.js
+++ Exchange/src/main.js
@@ -1,9 +1,10 @@
 
 import { getNavbar } from "./js/components/navbar.js";
+import { trending } from "./js/components/trending.js";
 
 
 
 const body = document.querySelector('body');
 
 body.innerHTML += getNavbar;
-console.log("hi")
+body.innerHTML += trending;
