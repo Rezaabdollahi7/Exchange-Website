1c65b195e03f3aa488b8f47fdef323c1c3a02811
babak ghaffari <babakghaffarii01@gmail.com>
2024-09-20 13:57:42 +0330

    {feat}: {home-wallet finished, chart styles edited}

--- Common/chartMaker/chartMaker.js
+++ Common/chartMaker/chartMaker.js
@@ -1,5 +1,5 @@
 export default function chart(ChartName, Data, Height, Width, Container){
-    const color = Data.at(-1) > Data.at(-2) ? "#00ff00" : Data.at(-1) < Data.at(-2) ? "#ff0000" : "#ffff00";
+    const color = Data.at(-1) > Data.at(-2) ? "#20D691" : Data.at(-1) < Data.at(-2) ? "#F65556" : "#ccd911";
     const additionClass = Data.at(-1) > Data.at(-2) ? "increase" : Data.at(-1) < Data.at(-2) ? 'decrease' : 'equal';
     const options = {
         series: [{
