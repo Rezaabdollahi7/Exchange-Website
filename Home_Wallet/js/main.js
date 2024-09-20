26f8a2f7d519ff1259c91e29bd0cb57fe5caa55f
babak ghaffari <babakghaffarii01@gmail.com>
2024-09-20 16:57:37 +0330

    last changes on wallet page

--- Home_Wallet/js/main.js
+++ Home_Wallet/js/main.js
@@ -3,6 +3,7 @@ import chart from "../../Common/chartMaker/chartMaker.js"
 const crypto = document.querySelector('#cryptoBtn');
 const nft = document.querySelector('#nftBtn');
 const chartContainers = document.querySelectorAll('.chartCont');
+const balance_status = document.querySelector("#balance-status");
 
 crypto.addEventListener('click', cryptoClick);
 nft.addEventListener('click', nftClick);
@@ -26,3 +27,16 @@ const data = [[-20,0,30,-134,80], [-20,30,-290,140,50,12,56,90,43], [-20,30,320,
 chartContainers.forEach((container, index) => {
     chart('bitcoin', data[index], 'auto', 2, container)
 })
+
+if (balance_status.textContent[0] === '+'){
+    balance_status.classList.add('up');
+    balance_status.classList.remove('down');
+    balance_status.childNodes[0].classList.add('fa-caret-up');
+    balance_status.childNodes[0].classList.remove('fa-caret-down');
+} else if (balance_status.textContent[0] === '-'){
+    balance_status.classList.add('down');
+    balance_status.classList.remove('up');
+    balance_status.childNodes[0].classList.add('fa-caret-down');
+    balance_status.childNodes[0].classList.remove('fa-caret-add');
+}
+
