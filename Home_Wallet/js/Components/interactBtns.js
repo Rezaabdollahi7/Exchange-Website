ecfe91a563e61de9e4cea1b750bcae31b6ac5555
babak ghaffari <babakghaffarii01@gmail.com>
2024-10-11 13:22:24 +0330

    {edit}: {buy subpage}

--- Home_Wallet/js/Components/interactBtns.js
+++ Home_Wallet/js/Components/interactBtns.js
@@ -1,18 +1,18 @@
 const interactBtns = `<div class="interaction-btns mt-5">
             <div class="interBtn_container">
-                <a href="./subpages/buy/index.html" class="wallet-btn"><img src="assets/Buy.svg" alt="Buy"></a>
+                <a href="#" class="wallet-btn"><img src="../Common/commonAssets/Buy.svg" alt="Buy"></a>
                 <span class="d-block text-center mt-3">Buy</span>
             </div>
             <div class="interBtn_container">
-                <a href="#" class="wallet-btn"><img src="assets/Receive.svg" alt="Receive"></a>
+                <a href="#" class="wallet-btn"><img src="../Common/commonAssets/Receive.svg" alt="Receive"></a>
                 <span class="d-block text-center mt-3">Receive</span>
             </div>
             <div class="interBtn_container">
-                <a href="#" class="wallet-btn"><img src="assets/Send.svg" alt="Send"></a>
+                <a href="#" class="wallet-btn"><img src="../Common/commonAssets/Send.svg" alt="Send"></a>
                 <span class="d-block text-center mt-3">Send</span>
             </div>
             <div class="interBtn_container">
-                <a href="#" class="wallet-btn"><img src="assets/Swap.svg" alt="Swap"></a>
+                <a href="#" class="wallet-btn"><img src="../Common/commonAssets/Swap.svg" alt="Swap"></a>
                 <span class="d-block text-center mt-3">Swap</span>
             </div>
         </div>`
--- Home_Wallet/js/Components/nav.js
+++ Home_Wallet/js/Components/nav.js
@@ -1,7 +1,7 @@
 const nav = `<nav class="mini-nav d-flex justify-content-between">
-            <img src="./assets/Logo.svg" alt="logo-image">
+            <img src="../Common/commonAssets/Logo.svg" alt="logo-image">
             <i class="setting-around rounded-pill" id="setting-button">
-                <img src="./assets/Settings.svg" alt="setting">
+                <img src="../Common/commonAssets/Settings.svg" alt="setting">
             </i>
         </nav>`
 
