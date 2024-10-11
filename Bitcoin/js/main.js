8ab3117404597ca7fa9539131c5927c11752ef60
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-10-11 17:04:55 +0330

    {Feat} : ( Add Chart v-1)

--- Bitcoin/js/main.js
+++ Bitcoin/js/main.js
@@ -11,3 +11,452 @@ close_x.addEventListener('click', ()=>{
     pop_back.classList.remove('show');
     pop_up.classList.remove('show');
 })
+
+var options = {
+    series: [{
+        name: 'line',
+        type: 'line',
+        data: [
+            {
+                x: new Date(1538778600000),
+                y: 6604
+            }, {
+                x: new Date(1538782200000),
+                y: 6602
+            }, {
+                x: new Date(1538814600000),
+                y: 6607
+            }, {
+                x: new Date(1538884800000),
+                y: 6620
+            }
+        ]
+    }, {
+        name: 'candle',
+        type: 'candlestick',
+        data: [
+            {
+                x: new Date(1538778600000),
+                y: [6629.81, 6650.5, 6623.04, 6633.33]
+            },
+            {
+                x: new Date(1538780400000),
+                y: [6632.01, 6643.59, 6620, 6630.11]
+            },
+            {
+                x: new Date(1538782200000),
+                y: [6630.71, 6648.95, 6623.34, 6635.65]
+            },
+            {
+                x: new Date(1538784000000),
+                y: [6635.65, 6651, 6629.67, 6638.24]
+            },
+            {
+                x: new Date(1538785800000),
+                y: [6638.24, 6640, 6620, 6624.47]
+            },
+            {
+                x: new Date(1538787600000),
+                y: [6624.53, 6636.03, 6621.68, 6624.31]
+            },
+            {
+                x: new Date(1538789400000),
+                y: [6624.61, 6632.2, 6617, 6626.02]
+            },
+            {
+                x: new Date(1538791200000),
+                y: [6627, 6627.62, 6584.22, 6603.02]
+            },
+            {
+                x: new Date(1538793000000),
+                y: [6605, 6608.03, 6598.95, 6604.01]
+            },
+            {
+                x: new Date(1538794800000),
+                y: [6604.5, 6614.4, 6602.26, 6608.02]
+            },
+            {
+                x: new Date(1538796600000),
+                y: [6608.02, 6610.68, 6601.99, 6608.91]
+            },
+            {
+                x: new Date(1538798400000),
+                y: [6608.91, 6618.99, 6608.01, 6612]
+            },
+            {
+                x: new Date(1538800200000),
+                y: [6612, 6615.13, 6605.09, 6612]
+            },
+            {
+                x: new Date(1538802000000),
+                y: [6612, 6624.12, 6608.43, 6622.95]
+            },
+            {
+                x: new Date(1538803800000),
+                y: [6623.91, 6623.91, 6615, 6615.67]
+            },
+            {
+                x: new Date(1538805600000),
+                y: [6618.69, 6618.74, 6610, 6610.4]
+            },
+            {
+                x: new Date(1538807400000),
+                y: [6611, 6622.78, 6610.4, 6614.9]
+            },
+            {
+                x: new Date(1538809200000),
+                y: [6614.9, 6626.2, 6613.33, 6623.45]
+            },
+            {
+                x: new Date(1538811000000),
+                y: [6623.48, 6627, 6618.38, 6620.35]
+            },
+            {
+                x: new Date(1538812800000),
+                y: [6619.43, 6620.35, 6610.05, 6615.53]
+            },
+            {
+                x: new Date(1538814600000),
+                y: [6615.53, 6617.93, 6610, 6615.19]
+            },
+            {
+                x: new Date(1538816400000),
+                y: [6615.19, 6621.6, 6608.2, 6620]
+            },
+            {
+                x: new Date(1538818200000),
+                y: [6619.54, 6625.17, 6614.15, 6620]
+            },
+            {
+                x: new Date(1538820000000),
+                y: [6620.33, 6634.15, 6617.24, 6624.61]
+            },
+            {
+                x: new Date(1538821800000),
+                y: [6625.95, 6626, 6611.66, 6617.58]
+            },
+            {
+                x: new Date(1538823600000),
+                y: [6619, 6625.97, 6595.27, 6598.86]
+            },
+            {
+                x: new Date(1538825400000),
+                y: [6598.86, 6598.88, 6570, 6587.16]
+            },
+            {
+                x: new Date(1538827200000),
+                y: [6588.86, 6600, 6580, 6593.4]
+            },
+            {
+                x: new Date(1538829000000),
+                y: [6593.99, 6598.89, 6585, 6587.81]
+            },
+            {
+                x: new Date(1538830800000),
+                y: [6587.81, 6592.73, 6567.14, 6578]
+            },
+            {
+                x: new Date(1538832600000),
+                y: [6578.35, 6581.72, 6567.39, 6579]
+            },
+            {
+                x: new Date(1538834400000),
+                y: [6579.38, 6580.92, 6566.77, 6575.96]
+            },
+            {
+                x: new Date(1538836200000),
+                y: [6575.96, 6589, 6571.77, 6588.92]
+            },
+            {
+                x: new Date(1538838000000),
+                y: [6588.92, 6594, 6577.55, 6589.22]
+            },
+            {
+                x: new Date(1538839800000),
+                y: [6589.3, 6598.89, 6589.1, 6596.08]
+            },
+            {
+                x: new Date(1538841600000),
+                y: [6597.5, 6600, 6588.39, 6596.25]
+            },
+            {
+                x: new Date(1538843400000),
+                y: [6598.03, 6600, 6588.73, 6595.97]
+            },
+            {
+                x: new Date(1538845200000),
+                y: [6595.97, 6602.01, 6588.17, 6602]
+            },
+            {
+                x: new Date(1538847000000),
+                y: [6602, 6607, 6596.51, 6599.95]
+            },
+            {
+                x: new Date(1538848800000),
+                y: [6600.63, 6601.21, 6590.39, 6591.02]
+            },
+            {
+                x: new Date(1538850600000),
+                y: [6591.02, 6603.08, 6591, 6591]
+            },
+            {
+                x: new Date(1538852400000),
+                y: [6591, 6601.32, 6585, 6592]
+            },
+            {
+                x: new Date(1538854200000),
+                y: [6593.13, 6596.01, 6590, 6593.34]
+            },
+            {
+                x: new Date(1538856000000),
+                y: [6593.34, 6604.76, 6582.63, 6593.86]
+            },
+            {
+                x: new Date(1538857800000),
+                y: [6593.86, 6604.28, 6586.57, 6600.01]
+            },
+            {
+                x: new Date(1538859600000),
+                y: [6601.81, 6603.21, 6592.78, 6596.25]
+            },
+            {
+                x: new Date(1538861400000),
+                y: [6596.25, 6604.2, 6590, 6602.99]
+            },
+            {
+                x: new Date(1538863200000),
+                y: [6602.99, 6606, 6584.99, 6587.81]
+            },
+            {
+                x: new Date(1538865000000),
+                y: [6587.81, 6595, 6583.27, 6591.96]
+            },
+            {
+                x: new Date(1538866800000),
+                y: [6591.97, 6596.07, 6585, 6588.39]
+            },
+            {
+                x: new Date(1538868600000),
+                y: [6587.6, 6598.21, 6587.6, 6594.27]
+            },
+            {
+                x: new Date(1538870400000),
+                y: [6596.44, 6601, 6590, 6596.55]
+            },
+            {
+                x: new Date(1538872200000),
+                y: [6598.91, 6605, 6596.61, 6600.02]
+            },
+        ]
+    }],
+    chart: {
+        height: 400,
+        zoom: {
+            enabled: true,
+            type: 'x',
+            autoScaleYaxis: true,
+            zoomedArea: {
+                fill: {
+                    color: '#90CAF9',
+                    opacity: 0.4
+                },
+                stroke: {
+                    color: '#0D47A1',
+                    opacity: 0.4,
+                    width: 1
+                }
+            }
+        },
+     
+        animations: {
+            enabled: true,
+            easing: 'easeinout',
+            speed: 800,
+            animateGradually: {
+                enabled: true,
+                delay: 250
+            },
+            dynamicAnimation: {
+                enabled: true,
+                speed: 350
+            }
+        },
+        toolbar: {
+            show: true,
+            offsetX: 0,
+            offsetY: 0,
+            tools: {
+                download: false,
+                selection: true,
+                zoom: true,
+                zoomin: true,
+                zoomout: true,
+                pan: false,
+                reset: true | '<img src="/static/icons/reset.png" width="20">',
+            },
+
+            autoSelected: 'zoom'
+        },
+      
+    },
+    legend: {
+        show: true,
+        showForSingleSeries: false,
+        showForNullSeries: true,
+        showForZeroSeries: true,
+        position: 'bottom',
+        horizontalAlign: 'center',
+        floating: false,
+        fontSize: '14px',
+        fontFamily: 'Helvetica, Arial',
+        fontWeight: 400,
+        formatter: undefined,
+        inverseOrder: false,
+        width: undefined,
+        height: undefined,
+        tooltipHoverFormatter: undefined,
+        customLegendItems: [],
+        offsetX: 0,
+        offsetY: 5,
+        labels: {
+            colors: undefined,
+            useSeriesColors: false
+        },
+        markers: {
+            size: 5,
+            shape: undefined,
+            strokeWidth: 1,
+            fillColors: undefined,
+            customHTML: undefined,
+            onClick: undefined,
+            offsetX: 0,
+            offsetY: 0
+        },
+        itemMargin: {
+            horizontal: 15,
+            vertical: 10
+        },
+        onItemClick: {
+            toggleDataSeries: true
+        },
+        onItemHover: {
+            highlightDataSeries: true
+        },
+    },
+    theme: {
+        mode: 'dark',
+        palette: 'palette6',
+        monochrome: {
+            enabled: true,
+            color: '#255aee',
+            shadeTo: 'light',
+            shadeIntensity: 0.65
+        },
+    },
+    plotOptions: {
+        candlestick: {
+            colors: {
+                upward: '#e1822e',
+                downward: '#386ac1'
+            },
+            
+        }
+    },
+    stroke: {
+        width: [3, 1]
+    },
+    tooltip: {
+        shared: true,
+        
+        custom: [function ({ seriesIndex, dataPointIndex, w }) {
+            return w.globals.series[seriesIndex][dataPointIndex]
+        }, function ({ seriesIndex, dataPointIndex, w }) {
+            var o = w.globals.seriesCandleO[seriesIndex][dataPointIndex]
+            var h = w.globals.seriesCandleH[seriesIndex][dataPointIndex]
+            var l = w.globals.seriesCandleL[seriesIndex][dataPointIndex]
+            var c = w.globals.seriesCandleC[seriesIndex][dataPointIndex]
+            return (
+                '<div class="apexcharts-tooltip-candlestick">' +
+                '<div>Open: <span class="value">' +
+                o +
+                '</span></div>' +
+                '<div>High: <span class="value">' +
+                h +
+                '</span></div>' +
+                '<div>Low: <span class="value">' +
+                l +
+                '</span></div>' +
+                '<div>Close: <span class="value">' +
+                c +
+                '</span></div>' +
+                '</div>'
+            )
+        }]
+    },
+    grid: {
+        position: 'back',
+        borderColor: '#273454',
+        strokeDashArray: 0,
+    },
+    grid: {
+        borderColor: '#273454',
+        xaxis: {
+            lines: {
+                show: true
+            }
+        }
+    },
+    xaxis: {
+        type: 'datetime',
+        lines: {
+            show: true,
+        },
+        min: undefined,
+        max: undefined,
+    },
+    yaxis: {
+        show: true,
+        lines: {
+            show: true,
+        },
+        showAlways: false,
+        floating: false,
+        decimalsInFloat: true,
+        labels: {
+            show: true,
+            align: 'left',
+            style: {
+                colors: [],
+                fontSize: '11px',
+                fontFamily: 'Helvetica, Arial, sans-serif',
+                fontWeight: 400,
+                cssClass: 'apexcharts-yaxis-label',
+            },
+            offsetX: -10,
+
+        },
+
+    },
+    fill: {
+        type: 'gradient',
+        gradient: {
+            shade: 'dark',
+            type: "vertical",
+            shadeIntensity: 0.5,
+            gradientToColors: undefined, // optional, if not defined - uses the shades of same color in series
+            inverseColors: true,
+            opacityFrom: 1,
+            opacityTo: 1,
+            stops: [0,30 , 100],
+            colorStops: []
+        }
+    }
+};
+
+var chart = new ApexCharts(document.querySelector("#chart"), options);
+chart.render();
+
+
+
+
+
