298d6b6b29f551e574af2c119772aeb5e3884401
babak ghaffari <babakghaffarii01@gmail.com>
2024-10-11 11:25:04 +0330

    {edit}: {home_wallet}

---
+++ Home_Wallet/js/Components/interactBtns.js
@@ -0,0 +1,20 @@
+const interactBtns = `<div class="interaction-btns mt-5">
+            <div class="interBtn_container">
+                <a href="./subpages/buy/index.html" class="wallet-btn"><img src="assets/Buy.svg" alt="Buy"></a>
+                <span class="d-block text-center mt-3">Buy</span>
+            </div>
+            <div class="interBtn_container">
+                <a href="#" class="wallet-btn"><img src="assets/Receive.svg" alt="Receive"></a>
+                <span class="d-block text-center mt-3">Receive</span>
+            </div>
+            <div class="interBtn_container">
+                <a href="#" class="wallet-btn"><img src="assets/Send.svg" alt="Send"></a>
+                <span class="d-block text-center mt-3">Send</span>
+            </div>
+            <div class="interBtn_container">
+                <a href="#" class="wallet-btn"><img src="assets/Swap.svg" alt="Swap"></a>
+                <span class="d-block text-center mt-3">Swap</span>
+            </div>
+        </div>`
+
+export default interactBtns
---
+++ Home_Wallet/js/Components/nav.js
@@ -0,0 +1,8 @@
+const nav = `<nav class="mini-nav d-flex justify-content-between">
+            <img src="./assets/Logo.svg" alt="logo-image">
+            <i class="setting-around rounded-pill" id="setting-button">
+                <img src="./assets/Settings.svg" alt="setting">
+            </i>
+        </nav>`
+
+export default nav;
