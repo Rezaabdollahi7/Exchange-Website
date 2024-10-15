c34ffc5a2cf0372c97c6fe74afff3366e547c131
babak ghaffari <babakghaffarii01@gmail.com>
2024-10-16 00:38:46 +0330

    {feat}: {setting}

---
+++ receive/js/components/nav.js
@@ -0,0 +1,10 @@
+const nav = `<nav class="mini-nav container p-0">
+            <div class="text-center position-relative">
+                <h1 class="head-name">Settings</h1>
+                <i class="setting-around rounded-pill position-absolute start-0 top-0" id="setting-button">
+                    <i class="fa-solid fa-arrow-left"></i>
+                </i>
+            </div>
+        </nav>`
+
+export default nav
