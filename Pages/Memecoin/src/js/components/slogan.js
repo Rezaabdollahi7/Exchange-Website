4652727247017f1fb133e391c5a2eec97755e12c
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-10-29 20:38:55 +0330

    {Feat} : ( MAGA page slogan section )

---
+++ Pages/Memecoin/src/js/components/slogan.js
@@ -0,0 +1,68 @@
+const slogan =
+    `
+    <!-- Main Content Section -->
+    <section id="slogan" class="main-content text-white mt-5 position-relative   w-100 overflow-hidden">
+        <video class="background-video position-absolute top-0 mt-1 " src="./assets/videos/USA-Flag-Waving.mp4" autoplay
+            loop muted playsinline></video>
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Left Column: Image -->
+                <div class="col-md-6 d-flex justify-content-center">
+                    <div class="eagle">
+                        <video src="./assets/videos/Eagle-Front.mp4" autoplay loop muted playsinline></video>
+                    </div>
+                </div>
+
+                <!-- Right Column: Text -->
+                <div class="col-md-6 px-4">
+                    <h1 class="main-title mb-4 ">MAGA Movement on the Blockchain.</h1>
+                    <div class="buttons mb-4 gap-3  d-flex flex-column d-sm-flex flex-sm-row  w-75 mx-auto mx-lg-0">
+                        <a href="#" class="btn btn-danger  px-4 py-2 fw-bold w-100 w-md-auto">Buy with Ethereum</a>
+                        <a href="#" class="btn btn-outline-light px-4 py-2 fw-bold w-100 w-md-auto">Buy with Solana</a>
+                    </div>
+                    <p class="description ">
+                        Pioneer of Political Finance (Politifi). We’ve raised more funds for U.S. Veterans and Child
+                        Trafficking Prevention than any other cryptocurrency on the market.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+        <div class="crypto-section mt-3">
+            <div class="container">
+                <div class="header">
+                    <div class="stars mx-auto">
+                        <img src="./assets/images/Star.png" alt="">
+                    </div>
+                    <h1 class="title mt-5">MAGA MEMECOIN</h1>
+                    <p class="subtitle mt-3">MAKE CRYPTO GREAT AGAIN</p>
+                </div>
+
+                <div class="addresses">
+                    <p class="address-item mx-auto"><span class="label">TAXED ETH</span> <code
+                            class="address-text mt-2 mt-md-2">0X576E2BED8F7B46D34016198911CDF9886F78BEA7</code></p>
+
+                    <p class="address-item"><span class="label">TAXLESS ETH</span> <code
+                            class="address-text mt-2 mt-md-2">0X6AA56E1D98B3805921C170EB4B3FE7D4FDA6D89B</code></p>
+
+                    <p class="address-item"><span class="label">SOL</span> <code
+                            class="address-text mt-2 mt-md-2">HaP8r3ksG76PhQLTqR8FYBeNiQpejcFbQmiHbg787Ut1</code></p>
+
+                    <p class="address-item"><span class="label">BASE</span> <code
+                            class="address-text mt-2 mt-md-2">0x57f5fbd3de65dfc0bd3630f732969e5fb97e6d37</code></p>
+
+                    <p class="address-item"><span class="label">BNB</span> <code
+                            class="address-text mt-2 mt-md-2">0x4eA98c1999575aaadfb38237Dd015c5E773F75a2</code></p>
+
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+
+
+
+`
+
+export { slogan }
