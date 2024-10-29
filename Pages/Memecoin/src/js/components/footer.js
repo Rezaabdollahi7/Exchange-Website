4d18116ee72e03d9fd89d517a0dee1f58bdea14d
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-10-29 21:09:26 +0330

    {Feat} : ( MAGA page footer section )

---
+++ Pages/Memecoin/src/js/components/footer.js
@@ -0,0 +1,42 @@
+const footer =
+    `
+    <footer class="footer text-center py-5 px-3 mt-4">
+        <div class="container">
+            <!-- Profile Image -->
+            <div class="footer-profile mb-3">
+                <img src="./assets/images/MAGA-Memecoin-Logo.png" alt="Profile Image"
+                    class="profile-img rounded-circle">
+            </div>
+
+            <!-- Privacy Button -->
+            <button class="btn btn-outline-light mb-3 px-4 mt-3">Privacy Policy</button>
+
+            <!-- Email -->
+            <p class="footer-email  fw-bolder">contact@magamemecoin.com</p>
+
+            <!-- Social Icons -->
+            <div class="footer-icons d-flex justify-content-center  my-4">
+                <img src="./assets/icons/Dextools-Logo-Icon.png" alt="Icon1" class="icon mx-2 mx-lg-3">
+                <img src="./assets/icons/X-Logo-Icon.png" alt="Icon2" class="icon mx-2 mx-lg-3">
+                <img src="./assets/icons/Telegram-Logo-Icon.png" alt="Icon3" class="icon mx-2 mx-lg-3">
+                <img src="./assets/icons/CoinMarketCap-Logo.png" alt="Icon4" class="icon mx-2 mx-lg-3">
+                <img src="./assets/icons/Coingecko-Logo.png" alt="Icon5" class="icon mx-2 mx-lg-3 ">
+                <img src="./assets/icons/Etherscan-Logo-Icon.png" alt="Icon6" class="icon mx-2 mx-lg-3">
+            </div>
+
+            <!-- Disclaimer -->
+            <p class="footer-disclaimer  mt-5">
+                Please be advised that MAGA Memecoin is a cryptocurrency created for entertainment purposes and
+                <strong class="text-decoration-underline">is not affiliated with or endorsed by Donald J.
+                    Trump</strong>. While MAGA Memecoin may
+                reference or
+                incorporate elements related Trump’s persona, image, or likeness, it does not imply any direct
+                endorsement,
+                partnership, or approval by Trump. Any resemblance or association between MAGA Memecoin and Donald J.
+                Trump
+                is purely coincidental and intended for satirical or humorous purposes.
+            </p>
+        </div>
+    </footer>
+`
+export { footer }
--- Pages/Memecoin/src/js/main.js
+++ Pages/Memecoin/src/js/main.js
@@ -1,7 +1,9 @@
 import { header } from "./components/header.js"
 import { slogan } from "./components/slogan.js"
+import { footer } from "./components/footer.js"
 
 let main = document.getElementById('memecoin-page')
 
 main.innerHTML += header;
 main.innerHTML += slogan;
+main.innerHTML += footer;
