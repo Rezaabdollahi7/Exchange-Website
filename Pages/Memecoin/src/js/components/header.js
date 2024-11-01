1df8605922916785b7d0cb28858aacf972010ead
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-01 21:27:22 +0330

    {Feat} : ( dynamic notif content )

--- Pages/Memecoin/src/js/components/header.js
+++ Pages/Memecoin/src/js/components/header.js
@@ -1,5 +1,9 @@
 const header =
     `
+        <div id="notification" class="notification shadow ">
+            <img src="./assets/images/MAGA-Memecoin-Logo-300x300.png" alt="icon" class="notification-icon">
+            <span class="notification-text">0.053 Maga claim at Exthiu995h8bu...</span>
+        </div>
     <!-- Header Section -->
     <header id="header" class="header text-center pt-5">
         <div class="container">
--- Pages/Memecoin/src/js/components/notif.js
+++ Pages/Memecoin/src/js/components/notif.js
@@ -1,13 +1,23 @@
-function notifAlert(){
+function notifAlert() {
+    const messages = [
+        "0.053 Maga claim at Exthiu995h8bu...",
+        "0.078 Maga claim at Retyu894g7h2...",
+        "0.065 Maga claim at Uiore655h9q...",
+        "0.091 Maga claim at Zxcvb982t6p..."
+    ];
+
     function showNotification() {
         const notification = document.getElementById("notification");
-
+        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+        notification.querySelector(".notification-text").textContent = randomMessage;
         notification.classList.add("show"); 
         setTimeout(() => {
             notification.classList.remove("show"); 
         }, 3000);
     }
 
-    setInterval(showNotification,4000);
+
+    setInterval(showNotification, 4000);
 }
-export {notifAlert}
+
+export { notifAlert };
