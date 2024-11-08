db20af5c790c7628b2e8595e04243e6e064a4105
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-08 13:40:02 +0330

    {Feat} : ( Add roulette )

--- Pages/Memecoin/src/js/components/slogan.js
+++ Pages/Memecoin/src/js/components/slogan.js
@@ -28,7 +28,17 @@ const slogan =
             </div>
 
         </div>
-        <div class="crypto-section mt-3">
+
+
+        <div class="roulette-wrapper">
+            <div class="selector"></div>
+            <div class="wheel"></div>
+        </div>
+
+        <button class="mb-4 spin-btn">Spin Roulette</button>
+
+
+        <div class="crypto-section ">
             <div class="container">
                 <div class="header">
                     <div class="stars mx-auto">
@@ -56,13 +66,130 @@ const slogan =
 
                 </div>
 
-            </div>
+            </div>            
         </div>
 
     </section>
 
 
+        <!-- Modal -->
+    <div class="modal fade" id="resultModal" tabindex="-1" aria-labelledby="resultModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-body text-dark">
+                    <div class="img-container d-flex justify-content-center align-items-center">
+                        <img class="mx-auto" src="./assets/images/MAGA-Memecoin-Logo.png" width="100" height="100"
+                            alt="">
+                    </div>
+                    <h3 class="mt-2 text-center fw-bold">
+                        <span id="winningAmount"></span> $TRUMP Tokens
+                    </h3>
+                    <p class="mt-4 connect-text">Connect Your Wallet to Claim Tokens!</p>
+                    <strong class="mt-2">
+                        Please connect one of the following wallets:
+                    </strong>
+                    <ul>
+                        <li class="mt-1">MetaMask</li>
+                        <li class="mt-1">Trust Wallet</li>
+                        <li class="mt-1">coinbase Wallet</li>
+                    </ul>
+                    <strong class="mt-2">Important:</strong>
+                    <ul>
+                        <li>Ensure you have the minimum required balance.</li>
+                        <li>After receiving your tokens, add the following token: </li>
+                    </ul>
+                    <strong class="mt-2">Token Address:</strong>
+                    <span class="bg-secondary px-2 py-1 ">0x576e2BeD8F7b46D34016198911Cdf9886f78bea7</span>
+                    <p class="mt-4 text-center h5">Get started now and enjoy your rewards!</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger text-white px-4 py-2 mx-auto"
+                        data-bs-dismiss="modal">Get Reward</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 `
 
-export { slogan }
+
+function spinRoulette() {
+    document.addEventListener("DOMContentLoaded", function () {
+        initWheel();
+
+        document.querySelector(".spin-btn").addEventListener("click", function () {
+            spinWheel();
+        });
+    });
+
+    function initWheel() {
+        var wheel = document.querySelector(".roulette-wrapper .wheel");
+
+        // مقادیر مختلف MAGA که روی کارت‌ها نمایش داده می‌شود
+        var magaValues = ["100 MAGA", "200 MAGA", "300 MAGA", "400 MAGA", "500 MAGA", "700 MAGA", "1000 MAGA", "1200 MAGA", "1400 MAGA", "1500 MAGA", "2000 MAGA", "2500 MAGA", "2700 MAGA", "3000 MAGA", "3500 MAGA", "100 MAGA", "200 MAGA", "300 MAGA", "400 MAGA", "500 MAGA", "700 MAGA", "1000 MAGA", "1200 MAGA", "1400 MAGA", "1500 MAGA", "2000 MAGA", "2500 MAGA", "2700 MAGA", "3000 MAGA", "3500 MAGA"];
+
+        // ایجاد یک ردیف برای هر کارت، هر ردیف فقط شامل یک کارت است
+        magaValues.forEach(function (value) {
+            var card = `
+            <div class='card'>
+                <img src="./assets/images/MAGA-Memecoin-Logo.png" alt="MAGA Logo">
+                <span>${value}</span>
+            </div>
+        `;
+            wheel.innerHTML += card;
+        });
+    }
+
+    function spinWheel() {
+        var wheel = document.querySelector(".roulette-wrapper .wheel");
+
+        // تعداد کارت‌ها در چرخ
+        var cardCount = 5;
+
+        // انتخاب یک موقعیت تصادفی از کارت‌های MAGA
+        var randomPosition = Math.floor(Math.random() * cardCount);
+
+        var cardWidth = 102; // عرض هر کارت به همراه حاشیه
+        var landingPosition = (cardCount * cardWidth * 2) + (randomPosition * cardWidth);
+
+        // تنظیم انیمیشن با حرکت تصادفی
+        var randomize = Math.floor(Math.random() * 50) - 25;
+        landingPosition = landingPosition + randomize;
+
+        // تنظیم انیمیشن
+        var object = {
+            x: Math.floor(Math.random() * 50) / 100,
+            y: Math.floor(Math.random() * 20) / 100
+        };
+
+        wheel.style.transitionTimingFunction = 'cubic-bezier(0,' + object.x + ',' + object.y + ',1)';
+        wheel.style.transitionDuration = '4s';
+        wheel.style.transform = 'translateX(-' + landingPosition + 'px)';
+
+        // پس از پایان چرخش، مقدار برنده شده را نمایش می‌دهیم
+        setTimeout(function () {
+            // شناسایی موقعیت نهایی چرخش
+            var wheelPosition = Math.abs(parseFloat(wheel.style.transform.replace("translateX(", "").replace("px)", "")));
+            var cardWidth = 100; // عرض هر کارت
+
+            // شناسایی کارت برنده بر اساس موقعیت چرخ
+            var winningCardIndex = Math.floor(wheelPosition / cardWidth) % 5;
+
+            // مقدار برنده شده
+            var cards = document.querySelectorAll('.roulette-wrapper .wheel .card');
+            var winningValue = cards[winningCardIndex].querySelector('span').innerText;
+
+            // مقدار برنده شده را به نمایش بگذار
+            document.getElementById('winningAmount').innerText = winningValue;
+
+            // نمایش مدال
+            var modal = new bootstrap.Modal(document.getElementById('resultModal'));
+            modal.show();
+        }, 4000); // مدت زمان انیمیشن چرخش
+    }
+
+}
+
+
+export { slogan, spinRoulette }
--- Pages/Memecoin/src/js/main.js
+++ Pages/Memecoin/src/js/main.js
@@ -1,10 +1,11 @@
 import { header } from "./components/header.js"
 import { slogan } from "./components/slogan.js"
+import { spinRoulette } from "./components/slogan.js"
 import { footer } from "./components/footer.js"
-import { accordionSwiperinit ,accordions } from "./components/accordions.js"
+import { accordionSwiperinit, accordions } from "./components/accordions.js"
 import { weekly_spaces } from "./components/weekly-spaces.js"
 import { copyEvent } from "./copy-events.js"
-import { tokenmicsSwiper ,tokenmics } from "./components/tokenmics.js"
+import { tokenmicsSwiper, tokenmics } from "./components/tokenmics.js"
 import { notifAlert } from "./components/notif.js"
 import { scrollCounter } from "./components/scroll-counter.js"
 
@@ -14,6 +15,7 @@ main.innerHTML += header;
 notifAlert();
 main.innerHTML += slogan;
 copyEvent();
+spinRoulette();
 main.innerHTML += tokenmics;
 scrollCounter();
 main.innerHTML += weekly_spaces;
