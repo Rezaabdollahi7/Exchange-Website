1ed335b48dc0748a549e4b03d8b19724619f70d8
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-15 19:24:06 +0330

    { Feat } : ( Dropdown items )

---
+++ Pages/smartDefi/src/js/main.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.dropdown-item').forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // گرفتن اطلاعات آیتم انتخاب شده
+        const selectedName = this.getAttribute('data-name');
+        const selectedIcon = this.getAttribute('data-icon');
+
+        // به‌روزرسانی محتوای دکمه اصلی
+        const button = document.querySelector('.network-btn');
+        button.innerHTML = `<img src="${selectedIcon}" alt="${selectedName} Icon" class="selected-icon"> ${selectedName}`;
+
+        // حذف کلاس active از تمام آیتم‌ها و اضافه کردن آن به آیتم انتخاب‌شده
+        document.querySelectorAll('.dropdown-item').forEach(el => el.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
