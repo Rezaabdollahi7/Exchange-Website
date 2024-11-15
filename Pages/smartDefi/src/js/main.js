a6688998da74216f5d8450dbd298992fa6d305ec
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-15 21:35:03 +0330

    {Feat} : { tooltips }

--- Pages/smartDefi/src/js/main.js
+++ Pages/smartDefi/src/js/main.js
@@ -15,3 +15,11 @@ document.querySelectorAll('.dropdown-item').forEach(item => {
         this.classList.add('active');
     });
 });
+
+// فعال‌سازی همه Tooltip‌ها در صفحه
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl);
+    });
+});
