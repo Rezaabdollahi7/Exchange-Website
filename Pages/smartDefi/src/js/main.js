3e9b4698a8fb0fab3b9b2cf1493245be860aaf09
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-17 01:04:33 +0330

    {Refactor} : { delete comments and optional styles }

--- Pages/smartDefi/src/js/main.js
+++ Pages/smartDefi/src/js/main.js
@@ -2,21 +2,21 @@ document.querySelectorAll('.dropdown-item').forEach(item => {
     item.addEventListener('click', function (e) {
         e.preventDefault();
 
-        // گرفتن اطلاعات آیتم انتخاب شده
+        // Get the selected item's data
         const selectedName = this.getAttribute('data-name');
         const selectedIcon = this.getAttribute('data-icon');
 
-        // به‌روزرسانی محتوای دکمه اصلی
+        // Update the main button content
         const button = document.querySelector('.network-btn');
         button.innerHTML = `<img src="${selectedIcon}" alt="${selectedName} Icon" class="selected-icon"> ${selectedName}`;
 
-        // حذف کلاس active از تمام آیتم‌ها و اضافه کردن آن به آیتم انتخاب‌شده
+        // Remove the active class from all items and add it to the selected item
         document.querySelectorAll('.dropdown-item').forEach(el => el.classList.remove('active'));
         this.classList.add('active');
     });
 });
 
-// فعال‌سازی همه Tooltip‌ها در صفحه
+// Enable all tooltips on the page
 document.addEventListener('DOMContentLoaded', function () {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
     tooltipTriggerList.forEach(function (tooltipTriggerEl) {
@@ -24,19 +24,18 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-
-// فعال‌سازی همه Popoverها در صفحه
+// Enable all popovers on the page
 document.addEventListener('DOMContentLoaded', function () {
-    // ذخیره کردن تمام Popover ها در متغیری برای مدیریت
+    // Store all popovers in a variable for management
     var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
     var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
         return new bootstrap.Popover(popoverTriggerEl);
     });
 
-    // بستن Popover زمانی که خارج از آن کلیک می‌شود
+    // Close popovers when clicking outside of them
     document.addEventListener('click', function (event) {
         popoverList.forEach(function (popover) {
-            // اگر عنصر کلیک شده داخل Popover یا آیکون trigger نباشد، Popover را ببندید
+            // If the clicked element is not inside the popover or trigger icon, hide the popover
             if (!popover._element.contains(event.target) && !event.target.closest('[data-bs-toggle="popover"]')) {
                 popover.hide();
             }
